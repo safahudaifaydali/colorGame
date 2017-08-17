402e21f28b6823ad070d559a02cb0afd7369a7a7
safa <safafaydali@gmail.com>
2017-08-17 15:01:08 +0300

    Initial numbers randomized.

--- colorGame.js
+++ colorGame.js
@@ -1,16 +1,10 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)",
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,7 +18,8 @@ for (var i = 0; i < squares.length; i++) {
 		if(clickedColor === pickedColor){
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Correct!";
-			changeColor(pickedColor);
+			changeColor(clickedColor);
+			h1.style.backgroundColor = clickedColor;
 		} else {
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
@@ -36,4 +31,24 @@ function changeColor(color){
 	for (var i = 0; i < squares.length; i++) {
 		squares[i].style.backgroundColor = color;
 	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num){
+	var result = [];
+	for (var i = 0; i < num; i++) {
+		result.push(randomColor());
+	}
+	return result;
+}
+
+function randomColor(){
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
